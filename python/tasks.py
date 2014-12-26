67b0c1624b293065f2872b952a11167081744848
Gavin Noronha <gnoronha@financialengines.com>
2014-12-26 10:01:55 -0800

    Added Redis locking

--- python/tasks.py
+++ python/tasks.py
@@ -3,6 +3,8 @@ import requests
 from celery import Celery
 from celery.utils.log import get_task_logger
 
+import redis
+
 logger = get_task_logger(__name__)
 
 app = Celery('tasks', broker='redis://localhost:6379/0',
@@ -17,7 +19,13 @@ def add(x, y):
 
 
 @app.task
-def count_words_at_url(url):
-    resp = requests.get(url)
+def count_words_at_url(url, id=-1):
+    if id >= 0:
+        with redis.Redis(host='localhost', port=6379).lock('id_%s' % id):
+            logger.warning('Used id_%s' % id)
+            resp = requests.get(url)
+    else:
+        resp = requests.get(url)
+
     logger.info('Getting info for the URL {0}'.format(url))
     return len(resp.text.split())
