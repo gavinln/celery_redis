642deba03826ebe93c69bb91381bc03c7b69a5ad
Gavin Noronha <gavinln@hotmail.com>
2015-06-23 21:39:57 -0700

    updated to use classes

--- python/redis_lock/rlock_test.py
+++ python/redis_lock/rlock_test.py
@@ -6,13 +6,12 @@ import time
 
 def worker(num):
     """thread worker function"""
-    print 'Worker:', num
-    lock()
+    lock(num)
     return
 
 if __name__ == '__main__':
     jobs = []
-    for i in range(2):
+    for i in range(6):
         p = multiprocessing.Process(target=worker, args=(i,))
         jobs.append(p)
         p.start()
